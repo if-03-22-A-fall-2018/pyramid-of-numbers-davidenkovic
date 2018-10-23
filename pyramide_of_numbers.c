654f9e17701281d1368d18231d542cbedff6e2fc
Ignjatovic David <Ignjatovicdavid1@gmail.com>
2018-10-23 10:53:03 +0200

    not finished

---
+++ pyramide_of_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
+
+char[] GetNumber(){
+  char[] inputOfUser;
+  printf("Enter number: ", );
+  scanf("%s", inputOfUser);
+  return inputOfUser;
+}
